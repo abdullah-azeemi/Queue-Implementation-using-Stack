1a0b2b38435fa64eae0375bb9176920ee0541409
Abdullah Musharaf <125180167+abdullah-azeemi@users.noreply.github.com>
2023-09-05 23:10:20 +0500

    Create MyQueue.h

---
+++ MyQueue.h
@@ -0,0 +1,67 @@
+#pragma once
+#include<iostream>
+#include<stdexcept>
+#include<stack>
+using namespace std;
+
+template<typename T>
+class MyQueue {
+	stack<T> s1;
+	stack<T> s2;
+
+public:
+	
+	MyQueue() {
+
+	}
+	void enque(T val) {
+		s1.push(val);
+	}
+	T deque() {
+
+		if (s1.empty() && s2.empty()) {
+			throw exception("Queue is empty ...");
+		}
+		int x;
+		if (!s2.empty()) {
+			x = s2.top();
+			s2.pop();
+			return x;
+		}
+		else {
+			while (!s1.empty()) {
+				s2.push(s1.top());
+				s1.pop();
+			}
+			x = s2.top();
+			s2.pop();
+			return x;
+		}
+	}
+	T peek() {  //--- Peek is almost the same as deque, if we do pop() the elemnts
+
+		if (s1.empty() && s2.empty()) {
+			throw exception("Queue is empty ...");
+		}
+		int x;
+		if (!s2.empty()) {
+			x = s2.top();
+			return x;
+		}
+		else {
+			while (!s1.empty()) {
+				s2.push(s1.top());
+				s1.pop();
+			}
+			x = s2.top();
+			return x;
+		}
+	}
+
+	bool empty() {
+		return (s1.empty() && s2.empty());
+	}
+	int size() {
+		return s1.size() + s2.size();
+	}
+};
